ecb6c7ce65d4515eaa615155f3f78f85ccf66497
Robert Dennison <rdennison11@gmail.com>
2023-02-13 12:37:29 +0000

    Remove unused step particle function

--- source/FluidSim/FluidSim/App/Particle2D.cpp
+++ source/FluidSim/FluidSim/App/Particle2D.cpp
@@ -13,24 +13,9 @@ Particle2D::Particle2D(const glm::vec2& inPosition, const glm::vec2& inVelocity,
 	mForceAccumulator = { 0.0f, g};
 }
 
-void Particle2D::StepParticle(float deltaTime)
-{
-	mAcceleration = mForceAccumulator * mMass;
-	// mVelocity += mAcceleration * deltaTime;
-	mPosition += mVelocity * deltaTime;// +mAcceleration * deltaTime * deltaTime;
-	
-	float g = -9.8f * mMass; // Downwards force due to gravity;
-
-	mForceAccumulator = { 0.0f, g }; // Always applly gravitational force.
-}
-
 void Particle2D::StepRK3(float deltaTime, const glm::vec2& K1, const glm::vec2& K2, const glm::vec2& K3)
 {
 	float scaledDt = deltaTime / 9.0f;
 
 	mPosition += (2.0f * scaledDt * K1) + (3.0f * scaledDt * K2) + (4.0f * scaledDt * K3);
-
-	mAcceleration = mForceAccumulator * mMass;
-	float g = -9.8f * mMass; // Downwards force due to gravity;
-	mForceAccumulator = { 0.0f, g }; // Always applly gravitational force.
 }
--- source/FluidSim/FluidSim/App/Particle2D.h
+++ source/FluidSim/FluidSim/App/Particle2D.h
@@ -11,7 +11,6 @@ public:
 
 	Particle2D(const glm::vec2& inPosition, const glm::vec2& inVelocity = glm::vec2(0.0f, 0.0f), float inMass = 1.0f, float inRadius = 0.1f);
 
-	void StepParticle(float deltaTime);
 	void StepRK3(float deltaTime, const glm::vec2& K1, const glm::vec2& K2, const glm::vec2& K3);
 
 	const glm::vec2& GetPosition() const { return mPosition; }
@@ -19,6 +18,8 @@ public:
 
 	const glm::vec2& GetVelocity() const { return mVelocity; }
 	void SetVelocity(const glm::vec2& inVel) { mVelocity = inVel; }
+	void SetXVelocity(float inXVel) { mVelocity.x = inXVel; }
+	void SetYVelocity(float inYVel) { mVelocity.y = inYVel; }
 
 	const glm::vec2& GetAcceleration() const { return mAcceleration; }
 	void SetAcceleration(const glm::vec2& inAcc) { mAcceleration = inAcc; }
@@ -30,5 +31,5 @@ private:
 	glm::vec2 mVelocity;
 	glm::vec2 mAcceleration;
 
-	glm::vec2 mForceAccumulator;
+	glm::vec2 mForceAccumulator; // For applying non-gravitational forces.
 };
