5b268ff3a28696ef8febeb0e45241e47291d2d50
Robert Dennison <rdennison11@gmail.com>
2023-03-20 15:37:50 +0000

    Store vec2 affine state rather than angular velocity

--- source/FluidSim/FluidSim/App/APICParticle2D.cpp
+++ source/FluidSim/FluidSim/App/APICParticle2D.cpp
@@ -1,13 +1,14 @@
 #include "APICParticle2D.h"
 
-APICParticle2D::APICParticle2D(const glm::dvec2& inPosition, const glm::dvec2& inVelocity, const glm::dvec3& inAngularVelocity, double inMass) 
+APICParticle2D::APICParticle2D(const glm::dvec2& inPosition, const glm::dvec2& inVelocity, double inMass)
 {
 	mPosition = inPosition;
 	mVelocity = inVelocity;
-	mAngularVelocity = inAngularVelocity;
 	mMass = inMass;
 	mAcceleration = { 0.0, -9.8f }; // Acceleration due to gravity
 
 	double g = -9.8f * mMass; // Downwards force due to gravity;
 	mForceAccumulator = { 0.0, g };
+
+	mAffineState = glm::dvec2(0.0);
 }
--- source/FluidSim/FluidSim/App/APICParticle2D.h
+++ source/FluidSim/FluidSim/App/APICParticle2D.h
@@ -9,12 +9,11 @@ class APICParticle2D : public Particle2D
 		APICParticle2D() = default;
 		~APICParticle2D() = default;
 
-		APICParticle2D(const glm::dvec2& inPosition, const glm::dvec2& inVelocity = glm::dvec2(0.0, 0.0), const glm::dvec3& inAngularVelocity = glm::dvec3(0.0, 0.0, 0.0), double inMass = 1.0);
+		APICParticle2D(const glm::dvec2& inPosition, const glm::dvec2& inVelocity = glm::dvec2(0.0), double inMass = 1.0);
 
-		const glm::dvec3& GetAngularVelocity() const { return mAngularVelocity; }
-		void SetAngularVelocity(const glm::dvec3& inAngVel) { mAngularVelocity = inAngVel; }
+		const glm::dvec2& GetAffineState() { return mAffineState; }
+		void SetAffineState(const glm::dvec2& inAff) { mAffineState = inAff; }
 
 	private:
-		// Vec 3 even in 2D fluid as angular momentum will point in the z direction.
-		glm::dvec3 mAngularVelocity;
+		glm::dvec2 mAffineState;
 };
